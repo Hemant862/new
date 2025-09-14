47eddd2b9c7df3235390a3961516ac7736786dfe
Hemant8625 <yog.esh.6g1a9@gmail.com>
2025-09-14 19:05:36 +0530

    new view added

--- pro1/App/urls.py
+++ pro1/App/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns=[
   path("show/",views.show),
+  path('lili/',views.lili)
 ]
--- pro1/App/views.py
+++ pro1/App/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 def show(request):
     return HttpResponse("<h1>Hello There.....</h1>")
 
+def lili(request):
+    return HttpResponse("<h1>Hello Lili 🌼🌼</h1>")
+
 # Create your views here.
